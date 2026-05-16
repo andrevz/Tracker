777fbcc854857b7992169aa1af8707f6bcb89659
Andre Vergara <andrevz11@icloud.com>
2026-05-16 16:34:24 -0400

    Fix leaflet references path

---
+++ src/Presentation/Tracker.WebApp/Tracker.WebApp/wwwroot/js/map-init.js
@@ -0,0 +1,9 @@
+export function initLeafletMap() {
+    console.log('Initializing Leaflet map...');
+
+    const map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(map);
+}
