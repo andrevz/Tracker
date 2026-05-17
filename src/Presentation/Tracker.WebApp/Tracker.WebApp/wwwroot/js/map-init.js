e67b66e6f8c4f7e3f8cf3d8dd355911864503bce
Andre Vergara <andrevz11@icloud.com>
2026-05-16 22:04:18 -0400

    Feature display stops on map

--- src/Presentation/Tracker.WebApp/Tracker.WebApp/wwwroot/js/map-init.js
+++ src/Presentation/Tracker.WebApp/Tracker.WebApp/wwwroot/js/map-init.js
@@ -1,7 +1,13 @@
+export function addStopMarker(latitude, longitude, name) {
+    L.marker([latitude, longitude])
+        .addTo(window.map)
+        .bindPopup(name);
+}
+
 export function initLeafletMap() {
-    const map = L.map('map').setView([-17.370791, -66.18359], 14);
+    window.map = L.map('map').setView([-17.370791, -66.18359], 14);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; OpenStreetMap contributors'
-    }).addTo(map);
+    }).addTo(window.map);
 }
